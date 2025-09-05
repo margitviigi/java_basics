cbcaf57b51e59bdca3a7249940dfe318b56401ad
Margit <margit.viigi@voco.ee>
2025-09-05 11:49:43 +0300

    Ex 18 - Grades and points

--- src/week1/Ex18.java
+++ src/week1/Ex18.java
@@ -10,15 +10,15 @@ public class Ex18 {
 
         if (number <= 29) {
             System.out.println("Failed");
-        } else if (number >= 30 && number <= 34) {
+        } else if (number <= 34) {
             System.out.println("1");
-        } else if (number >= 35 && number <= 39) {
+        } else if (number <= 39) {
             System.out.println("2");
-        } else if (number >= 40 && number <= 44) {
+        } else if (number <= 44) {
             System.out.println("3");
-        } else if (number >= 45 && number <= 49) {
+        } else if (number <= 49) {
             System.out.println("4");
-        } else if (number >= 50 && number <= 60) {
+        } else if (number <= 60) {
             System.out.println("5");
         } else {
             System.out.println("Input invalid");
