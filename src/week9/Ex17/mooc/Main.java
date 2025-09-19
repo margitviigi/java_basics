4a88cb28c2cc933bfc11b8ecbfe8eca803b8f1d6
Margit <margit.viigi@voco.ee>
2025-09-19 15:44:15 +0300

    Week 9 Ex 17 - First packages

---
+++ src/week9/Ex17/mooc/Main.java
@@ -0,0 +1,12 @@
+package week9.Ex17.mooc;
+import week9.Ex17.mooc.logic.ApplicationLogic;
+import week9.Ex17.mooc.ui.UserInterface;
+import week9.Ex17.mooc.ui.TextUserInterface;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserInterface ui = new TextUserInterface();
+        new ApplicationLogic(ui).execute(3);
+    }
+}
---
+++ src/week9/Ex17/mooc/logic/ApplicationLogic.java
@@ -0,0 +1,15 @@
+package week9.Ex17.mooc.logic;
+
+import week9.Ex17.mooc.ui.UserInterface;
+
+public class ApplicationLogic {
+    private UserInterface ui;
+    public ApplicationLogic(UserInterface ui){
+        this.ui = ui;
+    }
+    public void execute(int howManyTimes){
+        for(int i=0;i<howManyTimes;i++){
+            System.out.println("The application logic works");
+        }
+    }
+}
