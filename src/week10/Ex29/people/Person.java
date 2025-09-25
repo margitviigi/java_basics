a00ef16743d17850bc3beb134d07a7f1d77d460d
Margit <margit.viigi@voco.ee>
2025-09-25 09:43:38 +0300

    Week 10 Ex 29 - Person and their Heirs

---
+++ src/week10/Ex29/people/Person.java
@@ -0,0 +1,16 @@
+package week10.Ex29.people;
+
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + "\n  " + this.address;
+    }
+}
---
+++ src/week10/Ex29/people/Teacher.java
@@ -0,0 +1,16 @@
+package week10.Ex29.people;
+
+
+public class Teacher extends Person {
+    private int salary;
+
+    public Teacher(String name, String address, int salary) {
+        super(name, address);
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n  salary " + this.salary + " euros/month";
+    }
+}
